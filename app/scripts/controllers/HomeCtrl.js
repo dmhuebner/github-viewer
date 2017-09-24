c0b2fe37db22f424997b2a7fbce522e676b9b309
David M <davidmh.mail@gmail.com>
2017-09-24 18:28:25 -0500

    Add github api service and HomeCtrl search method

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,12 +1,34 @@
 (function() {
-  function HomeCtrl() {
+  function HomeCtrl(github, $log) {
     var home = this;
 
     home.test = "Testing Home Controller";
+
+    // Private Methods
+
+    var onUserComplete = function(data) {
+      home.currentUserObj = data;
+      home.searchError = null;
+      home.searchSuccess = true;
+    };
+
+    var onError = function(response) {
+      home.searchError = "There was an error processing your request";
+      home.searchSuccess = false;
+      $log.error(response.header);
+    };
+
+    // Controller Methods
+
+    home.search = function(username) {
+      $log.info("Searching for " + username);
+      github.getUser(username)
+        .then(onUserComplete, onError);
+    }
 
   }
 
   angular
     .module('githubViewer')
-    .controller('HomeCtrl', [HomeCtrl]);
+    .controller('HomeCtrl', ['github', '$log', HomeCtrl]);
 })();
---
+++ app/scripts/services/github.js
@@ -0,0 +1,27 @@
+(function() {
+  function github($http) {
+
+    var getUser = function(username) {
+      return $http.get('https://api.github.com/users/' + username)
+          .then(function(response) {
+            return response.data;
+          });
+    };
+
+    var getRepos = function(user) {
+      return $http.get(user.repos_url)
+        .then(function(response) {
+          return response.data;
+        });
+    };
+
+    return {
+      getUser: getUser,
+      getRepos: getRepos
+    }
+  }
+
+  angular
+    .module('githubViewer')
+    .factory('github', ['$http', github]);
+})();
